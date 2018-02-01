0e826928c08befbb6256ecdfb0b8e6ee48152446
lee <wzqnls@gmail.com>
2018-02-01 16:19:18 +0800

    add selection_sort

---
+++ 02-Sorting-Basic/selection_sort.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/2/1 15:58
+# @Author  : Lee
+# @File    : selection_sort.py
+# @Software: PyCharm
+
+from utils.common import swap
+
+
+def selection_sort(lists):
+    length = len(lists)
+
+    for i in range(length):
+        min_index = i
+        for j in range(i+1, length):
+            if lists[min_index] > lists[j]:
+                min_index = j
+        swap(lists, min_index, i)
+    return lists
+
+if __name__ == '__main__':
+    pass
