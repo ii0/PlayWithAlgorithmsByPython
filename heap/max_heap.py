56ad870e9c9f00283ff7d8c3654d860174887fe9
Lee <wzqnls@gmail.com>
2018-02-04 15:36:13 +0800

    add function shift_down for max_heap

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -30,6 +30,18 @@ class MaxHeap(object):
     def swap(self, i, j):
         self.data[i], self.data[j] = self.data[j], self.data[i]
 
+    def extract_max(self):
+        assert self.count > 0
+        result = self.data[1]
+        self.swap(1, self.count)
+        self.count -= 1
+        self.shift_down(1)
+        return result
+
+    def get_max(self):
+        assert self.count > 0
+        return self.data[1]
+
     """
     以下为核心辅助函数
     """
@@ -38,18 +50,29 @@ class MaxHeap(object):
         """
         此函数中 // 符号代表整除，结果为整数
         :param k: 节点位置
-        :return:
         """
         while k <= self.count and self.data[k] > self.data[k//2]:
             self.swap(k, k//2)
             k //= 2
 
+    def shift_down(self, k):
+        while 2 * k <= self.count:
+            j = 2 * k
+            # 判断右子树是否存在并对左右子节点进行比较
+            if j + 1 <= self.count and self.data[j] < self.data[j+1]:
+                j += 1
+            if self.data[k] < self.data[j]:
+                self.swap(k, j)
+                k = j
+            else:
+                break
+
 
 if __name__ == '__main__':
     max_heap = MaxHeap(100)
-    print(max_heap.size())
     for i in range(50):
         max_heap.insert(int(random.random() * 100))
-    print(max_heap.size())
-
-
+    lists = []
+    for i in range(50):
+        lists.append(max_heap.extract_max())
+    print(lists)
