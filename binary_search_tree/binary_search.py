8d61eafc9d7d1f248d36e53134e55e974f397f8a
Lee <wzqnls@gmail.com>
2018-02-07 23:10:02 +0800

    add binary_search(递归实现)

--- binary_search_tree/binary_search.py
+++ binary_search_tree/binary_search.py
@@ -21,6 +21,22 @@ def binary_search(lists, target):
     return -1
 
 
+def binary_search2(lists, target, *args):
+    if len(args):
+        l = args[0]
+        r = args[1]
+        mid = l + (r - l) // 2
+        if lists[mid] == target:
+            return mid
+        elif lists[mid] < target:
+            return binary_search2(lists, target, mid + 1, r)
+        else:
+            return binary_search2(lists, target, l, mid - 1)
+    else:
+        return binary_search2(lists, target, 0, len(lists) - 1)
+
+
 if __name__ == '__main__':
     test_lists = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     print(binary_search(test_lists, 6))
+    print(binary_search2(test_lists, 8))
